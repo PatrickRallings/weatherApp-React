58705a2be2eb7e98771b79713c746377c6c8be7d
Patrick Rallings <patrickrallings@Patricks-MBP.localdomain>
2022-04-20 09:43:48 -0400

    Initial set up of structure

---
+++ app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopSection from './TopSection'
+import MainSection from './MainSection'
+
+export default function App() {
+  return (
+    <div className='appCont col-8 justify-content-center'>
+        <TopSection />
+        <MainSection />
+
+    </div>
+  )
+}
---
+++ app/src/components/TopSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TopSection() {
+  return (
+    <div className="col" id="appTop">
+      <div className="alignLeft">
+        <input
+          style={{ height: "30px", margin: "10px 30px" }}
+          type="input"
+          placeholder="Enter zipcode or city name"
+        />
+        <button id="searchBtn">Search</button>
+      </div>
+    </div>
+  );
+}
